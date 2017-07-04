7c7b9bebbc900ae04a019f29ccdff5285883221a
Haolun <chshawkn@hotmail.com>
2017-07-04 15:47:22 +0800

    fix ci scripts

--- ci.sh
+++ ci.sh
@@ -7,34 +7,30 @@ if [ -f codesigning.asc.enc ] && [ "${TRAVIS_PULL_REQUEST}" == 'false' ]; then
 fi
 
 export BUILD_SITE_PATH_PREFIX="oss"
-export GRADLE_INIT_SCRIPT="https://github.com/home1-oss/oss-build/raw/develop/src/main/gradle/init-oss-lib.gradle"
+export GRADLE_INIT_SCRIPT="https://github.com/home1-oss/oss-build/raw/master/src/main/gradle/init-oss-lib.gradle"
 
 
 
 
 
 ### OSS CI CALL REMOTE CI SCRIPT BEGIN
-#if [ -z "${LIB_CI_SCRIPT}" ]; then LIB_CI_SCRIPT="https://github.com/home1-oss/oss-build/raw/develop/src/main/ci-script/lib_ci.sh"; fi
-if [ -z "${LIB_CI_SCRIPT}" ]; then LIB_CI_SCRIPT="http://gitlab.local:10080/home1-oss/oss-build/raw/develop/src/main/ci-script/lib_ci.sh"; fi
+if [ -z "${LIB_CI_SCRIPT}" ]; then LIB_CI_SCRIPT="https://github.com/home1-oss/oss-build/raw/master/src/main/ci-script/lib_ci.sh"; fi
+#if [ -z "${LIB_CI_SCRIPT}" ]; then LIB_CI_SCRIPT="http://gitlab.local:10080/home1-oss/oss-build/raw/master/src/main/ci-script/lib_ci.sh"; fi
 echo "eval \$(curl -s -L ${LIB_CI_SCRIPT})"
 eval "$(curl -s -L ${LIB_CI_SCRIPT})"
 #source src/main/ci-script/lib_ci.sh
 ### OSS CI CALL REMOTE CI SCRIPT END
 
+export ORIGINAL_GRADLE_PROPERTIES="${GRADLE_PROPERTIES}"
 SPRING_BOOT_VERSIONS=( "1.4.2.RELEASE" )
 
-gradle_action() {
-    export ORIGINAL_GRADLE_PROPERTIES="${GRADLE_PROPERTIES}"
-    for spring_boot_version in "${SPRING_BOOT_VERSIONS[@]}"; do
-        export GRADLE_PROPERTIES="${ORIGINAL_GRADLE_PROPERTIES} -PspringBootVersion=${spring_boot_version}"
-        gradle_$@
-    done
-}
-
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>> execute '${COMMANDS_WILL_PERFORM[@]}' on spring-boot '${SPRING_BOOT_VERSIONS[@]}' >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-for command in ${COMMANDS_WILL_PERFORM[@]}; do
-    echo ">>>>>>>>>>>>>>>>>>>> execute 'gradle_action ${command}' >>>>>>>>>>>>>>>>>>>>"
-    gradle_action ${command}
-    echo "<<<<<<<<<<<<<<<<<<<< done 'gradle_action ${command}' <<<<<<<<<<<<<<<<<<<<"
+for spring_boot_version in "${SPRING_BOOT_VERSIONS[@]}"; do
+    export GRADLE_PROPERTIES="${ORIGINAL_GRADLE_PROPERTIES} -PspringBootVersion=${spring_boot_version}"
+    for command in ${COMMANDS_WILL_PERFORM[@]}; do
+        echo ">>>>>>>>>>>>>>>>>>>> execute 'gradle_${command}' on spring-boot '${spring_boot_version}' >>>>>>>>>>>>>>>>>>>>"
+        gradle_${command}
+        echo "<<<<<<<<<<<<<<<<<<<< done 'gradle_${command}' on spring-boot '${spring_boot_version}' <<<<<<<<<<<<<<<<<<<<"
+    done
 done
 echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< done '${COMMANDS_WILL_PERFORM[@]}' on spring-boot '${SPRING_BOOT_VERSIONS[@]}' <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
